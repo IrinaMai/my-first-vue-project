6ac65dfd10d4d85c845db6e2dd59b623e6f0c0f4
Irena <irena.mali@speedandfunction.com>
2022-05-28 17:48:13 +0200

    added form

---
+++ src/components/ApartmentFilterForm.vue
@@ -0,0 +1,25 @@
+<template>
+<form class="form">
+<CustomSelect/>
+<CustomInput/>
+<CustomButton :type="submit"/>
+</form>
+</template>
+
+<script>
+import CustomButton from './share/CustomButton.vue';
+import CustomInput from './share/CustomInput.vue';
+import CustomSelect from './share/CustomSelect.vue';
+
+    export default {
+        name: 'ApartmentFilterForm'
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.form{
+    display: flex;
+    outline: 1px solid tomato;
+}
+</style>
---
+++ src/components/share/CustomButton.vue
@@ -0,0 +1,39 @@
+<template>
+<button class="custom-button" :type="type">{{name}}</button>
+</template>
+
+<script>
+    export default {
+       name: "CustomButton",
+       props: {
+           name: {
+               type: String,
+               default: "Confirm"
+           },
+           type: {
+               type: String,
+               default: 'button'
+           }
+       }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "../../assets/scss/variables.scss";
+
+.custom-button {
+    min-height: 40px;
+    border: 2px solid $accent-color;
+    font-size: 18px;
+    outline: none;
+    line-height:inherit;
+    padding: 8px 15px;
+    color: $accent-color;
+
+    &:hover {
+        background-color: $accent-color;
+        color: white;
+    }
+}
+
+</style>
